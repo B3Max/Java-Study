e23cf9c82dbaa16e942c0474df5326193cc80907
B3Max <B3dMaxOsp@yandex.ru>
2021-10-04 21:37:44 +0300

    Initial Commit

--- src/work_21/Animation.java
+++ src/work_21/Animation.java
@@ -2,12 +2,20 @@ package work_21;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Animation extends JComponent {
-    Image image = new ImageIcon("work_21/0.png").getImage();
+public class Animation extends JComponent implements ActionListener {
+    Image image = new ImageIcon("src/work_21/pictures/0.png").getImage();
+    Timer t;
 
-    public void paint (Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
+    Animation() {
+        t = new Timer(100, this);
+        t.start();
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         g2.drawImage(image, 0, 0, null);
     }
 
@@ -15,11 +23,35 @@ public class Animation extends JComponent {
         Animation animation = new Animation();
         JFrame f = new JFrame("Animation");
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setSize(410, 310);
+        f.setSize(415, 340);
         f.setLocation(750, 300);
+        f.setResizable(false);
         f.add(new Animation());
         f.add(animation);
         f.setVisible(true);
-        //f.addKeyListener(animation);
+    }
+
+    int count = 1;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (count) {
+            case (0) -> image = new ImageIcon("src/work_21/pictures/0.png").getImage();
+            case (1) -> image = new ImageIcon("src/work_21/pictures/1.png").getImage();
+            case (2) -> image = new ImageIcon("src/work_21/pictures/2.png").getImage();
+            case (3) -> image = new ImageIcon("src/work_21/pictures/3.png").getImage();
+            case (4) -> image = new ImageIcon("src/work_21/pictures/4.png").getImage();
+            case (5) -> image = new ImageIcon("src/work_21/pictures/5.png").getImage();
+            case (6) -> image = new ImageIcon("src/work_21/pictures/6.png").getImage();
+            case (7) -> image = new ImageIcon("src/work_21/pictures/7.png").getImage();
+            case (8) -> image = new ImageIcon("src/work_21/pictures/8.png").getImage();
+            case (9) -> image = new ImageIcon("src/work_21/pictures/9.png").getImage();
+            case (10) -> image = new ImageIcon("src/work_21/pictures/10.png").getImage();
+
+        }
+        count++;
+        if (count == 11)
+            count = 0;
+        repaint();
     }
 }
