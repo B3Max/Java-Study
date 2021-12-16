f253b1ddb74d80f8d15232379873af6f4ef7c466
B3Max <B3dMaxOsp@yandex.ru>
2021-12-16 16:21:05 +0300

    Initial Commit

--- src/work_12/_2/TestAddress.java
+++ src/work_12/_2/TestAddress.java
@@ -4,8 +4,8 @@ public class TestAddress {
 
     public static void main(String[] args) {
         Address address1 = Address.commaSeparator("Россия, 26, Ставрополь, Киевская ул, 10, 2, 44");
-        Address address2 = Address.anySeparator("Россия; 26; Ставрополь; Киевская; 10; 2; 44");
-        Address address3 = Address.anySeparator("Россия 26 Ставрополь Киевская 10 2 44");
+        Address address2 = Address.anySeparator("Россия; 26; Ставрополь; Киевская ул; 10; 2; 44");
+        Address address3 = Address.anySeparator("Россия 26 Ставрополь Киевская ул 10 2 44");
 
         System.out.println(address1);
         System.out.println(address2);
---
+++ src/work_27/Solution.java
@@ -0,0 +1,36 @@
+package work_27;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Solution {
+    public static <E> HashSet<E> arrayToHashSet(E[] arr) {
+        if (arr == null)
+            throw new IllegalArgumentException("Null array!");
+        HashSet<E> s = new HashSet<>(arr.length);
+        Collections.addAll(s, arr);
+        return s;
+    }
+
+    public static <K, V> HashMap<K, V> arrayToHashMap(K[] arr, V[] vArr) {
+        if (arr == null || vArr == null)
+            throw new IllegalArgumentException("Null array!");
+        if (vArr.length < arr.length)
+            throw new IllegalArgumentException("Массив значений меньше массива ключей!");
+        HashMap<K, V> m = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            m.put(arr[i], vArr[i]);
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"Hello", "world", "!"};
+        HashSet<String> hs = arrayToHashSet(a);
+        System.out.println(hs);
+
+        HashMap<String, String> m = arrayToHashMap(a, new String[]{"H", "w", "!"});
+        System.out.println(m);
+    }
+}
